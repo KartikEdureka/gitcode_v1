6c3fa39aae9722ec63baf14f0b92724a8c26e1cd
KartikEdureka <kartikey.edureka25@gmail.com>
2022-05-22 00:01:25 +0530

    first commit

---
+++ src/GitDemo.java
@@ -0,0 +1,18 @@
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry ="yes";
+		if (hungry == "yes") {
+			System.out.println("Eat something");
+			}
+		
+		else {
+			System.out.println("do not eat anything");
+		}
+
+	} 
+
+}
